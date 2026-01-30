9b066c5ba660944f3d5dafcc79f150b0b85bbb05
fiberhomebot <rezhamata@gmail.com>
2026-01-30 22:35:08 +0700

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-// Shim to accept alternate env var names and start the main bot
-process.env.TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN || process.env.BOT_TOKEN;
-process.env.SHEET_ID = process.env.SHEET_ID || process.env.SPREADSHEET_ID;
-
-require('./bot-progres-psb.js');
